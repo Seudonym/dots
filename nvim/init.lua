f39df6f0f91c1b700e37970dc25d27bbb27ea9d3
Wahid Khan <wk170179@gmail.com>
2024-04-28 16:08:19 +0530

    nvchad new

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,67 +1,39 @@
--- init.lua
--- jj to escape
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
--- General settings
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.wrap = false
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.clipboard = "unnamedplus"
-
--- Indentation settings
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-
--- Enable mouse
-vim.o.mouse = 'a'
-
--- Enable line numbers
-vim.wo.number = true
-vim.wo.relativenumber = true
-
--- Enable 24bit color
-vim.opt.termguicolors = true
-
--- Disable cmdline
-vim.opt.cmdheight = 0
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- Set leader key
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
--- Load plugins
-local plugins = require("plugins")
-require("lazy").setup(plugins, opts)
+local lazy_config = require "configs.lazy"
 
--- Load mappings
-local load_mappings = require("utils.load_mappings")
-load_mappings(require("mappings.general"))
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
 
--- Copilot
-vim.keymap.set('i', '<M-l>', 'copilot#Accept("\\<CR>")', {
-    expr = true,
-    replace_keycodes = false
-})
-vim.g.copilot_no_tab_map = true
+  { import = "plugins" },
+}, lazy_config)
 
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
 
-vim.cmd("colorscheme nordfox")
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
--- nvim/lua/mappings/bufferline.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-    n = {
-        ['<Tab>'] = {'<cmd> bn <CR>', 'Go to next buffer'},
-        ['<S-Tab>'] = {'<cmd> bp <CR>', 'Go to prev buffer'},
-    }
-}
-
-return M
-
